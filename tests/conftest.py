62cd729db24c6218b82ffc56717b2174f36d8711
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-05-01 16:48:38 +0300

    add scope

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,3 +6,9 @@ def set_up():
     print("Login completed")
     yield
     print("Sign Out")
+
+@pytest.fixture(scope="module")
+def some():
+    print("Start")
+    yield
+    print("End")
--- tests/test_mail_1.py
+++ tests/test_mail_1.py
@@ -1,9 +1,9 @@
 import pytest
 
 
-def test_sending_mail_1(set_up):
+def test_sending_mail_1(set_up, some):
     print("Mail sent")
 
-def test_sending_mail_2(set_up):
+def test_sending_mail_2(set_up, some):
     print("Mail sent")
 #test_sending_mail_2()
