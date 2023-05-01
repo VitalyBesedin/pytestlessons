757ad6fadda3640cc46030a459c176722d7fcc05
VitalyBesedin <vitaly_besedin@hotmail.com>
2023-05-01 17:02:49 +0300

    initiation test_order.pyorder

---
+++ tests/test_order.py
@@ -0,0 +1,17 @@
+def test_method_1(set_up):
+    print("Method 1")
+
+def test_method_2(set_up):
+    print("Method 2")
+
+def test_method_2(set_up):
+    print("Method 3")
+
+def test_method_4(set_up):
+    print("Method 4")
+
+def test_method_5(set_up):
+    print("Method 5")
+
+def test_method_6(set_up):
+    print("Method 6")
